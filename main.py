c5abd4eeccc4a0de67be34c18e519e4831bdb28b
brifeb <brifeb@gmail.com>
2020-11-17 17:10:59 +0700

    Menambah beberapa contoh file dari remoteworker

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import requests
 
-print('Hello World!')
+nama = 'Azzam'
+print(f'Hello World! to {nama}')
+print('Hello World! to {}'.format(nama))
 
+nama_situs = 'ndoware'
 try:
+    url = f'https://{nama_situs}.com'
+    print(url)
     r = requests.get('https://ndoware.com')
     print(r.status_code)
     if r.status_code == 200:
+        # print()
         print(r.text)
 except Exception as e:
     print('Ada error: ', e)
